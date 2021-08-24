d3d4558c14083d8129e11d4265d9365e16029b68
jr-cast <castanos@pm.me>
2021-08-24 20:19:39 +0100

    adding zones.csv

--- post-process-data/post-process.py
+++ post-process-data/post-process.py
@@ -2,22 +2,30 @@ import pandas as pd
 
 # North
 df = pd.read_csv("eplusout.csv")
-
 df = df.drop(df.columns[[1, 2, 3, 5, 6]], axis=1) 
-
 df.insert(1, "northZone", "northZone")
-
-# df.columns = [''] * len(df.columns)
-
 df.to_csv('northZone.csv', header=False, index=False)
 
 # South
 df = pd.read_csv("eplusout.csv")
+df = df.drop(df.columns[[1, 3, 4, 5, 6]], axis=1) 
+df.insert(1, "southZone", "southZone")
+df.to_csv('southZone.csv', header=False, index=False)
 
-df = df.drop(df.columns[[1, 2, 3, 5, 6]], axis=1) 
+# East
+df = pd.read_csv("eplusout.csv")
+df = df.drop(df.columns[[1, 2, 4, 5, 6]], axis=1) 
+df.insert(1, "eastZone", "eastZone")
+df.to_csv('eastZone.csv', header=False, index=False)
 
-df.insert(1, "northZone", "northZone")
+# West
+df = pd.read_csv("eplusout.csv")
+df = df.drop(df.columns[[1, 2, 3, 4, 6]], axis=1) 
+df.insert(1, "westZone", "westZone")
+df.to_csv('westZone.csv', header=False, index=False)
 
-# df.columns = [''] * len(df.columns)
-
-df.to_csv('northZone.csv', header=False, index=False)
+# Core
+df = pd.read_csv("eplusout.csv")
+df = df.drop(df.columns[[1, 2, 3, 4, 5]], axis=1) 
+df.insert(1, "coreZone", "coreZone")
+df.to_csv('coreZone.csv', header=False, index=False)
