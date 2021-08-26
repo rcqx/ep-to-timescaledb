634a92a68a2d4c1691c2eefe82188a5ee348584f
jr-cast <castanos@pm.me>
2021-08-26 13:53:52 +0100

    testing timeseries

--- 5zone_bot_model/generate_bot.py
+++ 5zone_bot_model/generate_bot.py
@@ -1,4 +1,5 @@
 from rdflib import RDF, RDFS, OWL, Namespace, Graph, Literal
+from rdflib.term import BNode
 
 g = Graph()
 
@@ -10,8 +11,6 @@ BOT = Namespace("https://w3c-lbd-cg.github.io/bot#")
 g.bind("bot", BOT)
 UNIT = Namespace("http://qudt.org/vocab/unit/")
 g.bind("unit", UNIT)
-
-# SIM = Namespace("http://example.com/Simulation#")
 
 # site
 g.add((BLDG.Site, RDF.type, BOT.Site))
@@ -31,16 +30,12 @@ g.add((BLDG.ZONE_S, RDF.type, BOT.Zone))
 g.add((BLDG.ZONE_E, RDF.type, BOT.Zone))
 g.add((BLDG.ZONE_W, RDF.type, BOT.Zone))
 g.add((BLDG.ZONE_C, RDF.type, BOT.Zone))
-
 # window
 g.add((BLDG.window, RDF.type, BOT.Element))
-
 # glassdoor
 g.add((BLDG.glassdoor, RDF.type, BOT.Element))
-
 # wall
 g.add((BLDG.wall, RDF.type, BOT.Element))
-
 # ceiling
 g.add((BLDG.ceiling, RDF.type, BOT.Element))
 
@@ -59,81 +54,52 @@ g.add((BLDG.office_building, BOT.hasStorey, BLDG.office))
 # relations for North zone
 g.add((BLDG.office, BOT.hasSpace, BLDG.office_N))
 g.add((BLDG.ZONE_N, BOT.hasSpace, BLDG.office_N))
-g.add((BLDG.ZONE_N, RDFS.label, Literal("North Zone Temperature")))
-g.add((BLDG.ZONE_N, RDF.value, UNIT.DEG))
-g.add((BLDG.ZONE_N, RDF.value, UNIT.DEG))
-
-
-
-
-g.add((BLDG.ZONE_N, BOT.hasElement, BLDG.thermostatN))
-g.add((BLDG.ZONE_N, BOT.hasElement, BLDG.RHSensor_N))
-g.add((BLDG.office_N, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_N, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_N, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_N, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_N, BOT.hasElement, BLDG.ceiling))
-g.add((BLDG.office_N, BOT.hasElement, BLDG.glassdoor))
-g.add((BLDG.office_N, BOT.hasElement, BLDG.window))
-
+g.add((BLDG.ZONE_N, BOT.hasElement, BLDG["TS_North"]))
+g.add((BLDG["TS_North"], RDFS.label, Literal("Room temperature measure in North Office")))
+g.add((BLDG["TS_North"], UNIT.applicableUnit, UNIT.DEG_C))
+ts_ref_N = BNode()
+g.add((ts_ref_N, BLDG.hasTimeseriesId, Literal("northZone")))
+g.add((BLDG.temp_sensor_N, BLDG.timeseries, ts_ref_N))
 
 # relations for South zone
 g.add((BLDG.office, BOT.hasSpace, BLDG.office_S))
 g.add((BLDG.ZONE_S, BOT.hasSpace, BLDG.office_S))
 g.add((BLDG.ZONE_S, BOT.hasElement, BLDG["TS_South"]))
-g.add((BLDG.office_S, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_S, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_S, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_S, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_S, BOT.hasElement, BLDG.ceiling))
-g.add((BLDG.office_S, BOT.hasElement, BLDG.glassdoor))
-g.add((BLDG.office_S, BOT.hasElement, BLDG.window))
+g.add((BLDG["TS_South"], RDFS.label, Literal("Room temperature measure in South Office")))
+g.add((BLDG["TS_South"], UNIT.applicableUnit, UNIT.DEG_C))
+ts_ref_S = BNode()
+g.add((ts_ref_S, BLDG.hasTimeseriesId, Literal("southZone")))
+g.add((BLDG.temp_sensor_S, BLDG.timeseries, ts_ref_S))
+
 # relations for East zone
 g.add((BLDG.office, BOT.hasSpace, BLDG.office_E))
 g.add((BLDG.ZONE_E, BOT.hasSpace, BLDG.office_E))
 g.add((BLDG.ZONE_E, BOT.hasElement, BLDG["TS_East"]))
-g.add((BLDG.office_E, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_E, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_E, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_E, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_E, BOT.hasElement, BLDG.ceiling))
-g.add((BLDG.office_E, BOT.hasElement, BLDG.window))
+g.add((BLDG["TS_East"], RDFS.label, Literal("Room temperature measure in East Office")))
+g.add((BLDG["TS_East"], UNIT.applicableUnit, UNIT.DEG_C))
+ts_ref_E = BNode()
+g.add((ts_ref_E, BLDG.hasTimeseriesId, Literal("eastZone")))
+g.add((BLDG.temp_sensor_E, BLDG.timeseries, ts_ref_E))
+
 # relations for West zone
 g.add((BLDG.office, BOT.hasSpace, BLDG.office_W))
 g.add((BLDG.ZONE_W, BOT.hasSpace, BLDG.office_W))
 g.add((BLDG.ZONE_W, BOT.hasElement, BLDG["TS_West"]))
-g.add((BLDG.office_W, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_W, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_W, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_W, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_W, BOT.hasElement, BLDG.ceiling))
-g.add((BLDG.office_W, BOT.hasElement, BLDG.window))
+g.add((BLDG["TS_West"], RDFS.label, Literal("Room temperature measure in West Office")))
+g.add((BLDG["TS_West"], UNIT.applicableUnit, UNIT.DEG_C))
+ts_ref_W = BNode()
+g.add((ts_ref_W, BLDG.hasTimeseriesId, Literal("westZone")))
+g.add((BLDG.temp_sensor_W, BLDG.timeseries, ts_ref_W))
+
 # relations for Core zone
 g.add((BLDG.office, BOT.hasSpace, BLDG.office_C))
 g.add((BLDG.ZONE_C, BOT.hasSpace, BLDG.office_C))
 g.add((BLDG.ZONE_C, BOT.hasElement, BLDG["TS_Core"]))
-g.add((BLDG.office_C, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_C, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_C, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_C, BOT.hasElement, BLDG.wall))
-g.add((BLDG.office_C, BOT.hasElement, BLDG.ceiling))
-# adjacent zones for N
-g.add((BLDG.office_N, BOT.adjacentZone, BLDG.office_E))
-g.add((BLDG.office_N, BOT.adjacentZone, BLDG.office_W))
-# adjacent zones for S
-g.add((BLDG.office_S, BOT.adjacentZone, BLDG.office_E))
-g.add((BLDG.office_S, BOT.adjacentZone, BLDG.office_W))
-# adjacent zones for W
-g.add((BLDG.office_W, BOT.adjacentZone, BLDG.office_N))
-g.add((BLDG.office_W, BOT.adjacentZone, BLDG.office_S))
-# adjacent zones for E
-g.add((BLDG.office_E, BOT.adjacentZone, BLDG.office_N))
-g.add((BLDG.office_E, BOT.adjacentZone, BLDG.office_S))
-# adjacent zones for C
-g.add((BLDG.office_C, BOT.adjacentZone, BLDG.office_N))
-g.add((BLDG.office_C, BOT.adjacentZone, BLDG.office_S))
-g.add((BLDG.office_C, BOT.adjacentZone, BLDG.office_E))
-g.add((BLDG.office_C, BOT.adjacentZone, BLDG.office_W))
+g.add((BLDG["TS_Core"], RDFS.label, Literal("Room temperature measure in Core Office")))
+g.add((BLDG["TS_Core"], UNIT.applicableUnit, UNIT.DEG_C))
+ts_ref_C = BNode()
+g.add((ts_ref_C, BLDG.hasTimeseriesId, Literal("coreZone")))
+g.add((BLDG.temp_sensor_C, BLDG.timeseries, ts_ref_C))
 
-with open("5Zone_bot.ttl", "wb") as f:
+with open("EP_timeseries.ttl", "wb") as f:
     f.write(g.serialize(format="ttl", encoding='UTF-8'))
